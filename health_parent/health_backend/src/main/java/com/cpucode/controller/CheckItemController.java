8f648af748749ea0cb2981efaee287812be8c4ac
cpuCode <923992029@qq.com>
2021-03-29 13:20:57 +0800

    Controller

---
+++ health_parent/health_backend/src/main/java/com/cpucode/controller/CheckItemController.java
@@ -0,0 +1,37 @@
+package com.cpucode.controller;
+
+import com.cpucode.constant.MessageConstant;
+import com.cpucode.entity.Result;
+import com.cpucode.pojo.CheckItem;
+import com.cpucode.service.CheckItemService;
+import jdk.nashorn.internal.ir.annotations.Reference;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *  体检检查项管理
+ *
+ * @author : cpucode
+ * @date : 2021/3/29
+ * @time : 12:59
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+@RestController
+@RequestMapping("/checkitem")
+public class CheckItemController {
+    @Reference
+    private CheckItemService checkItemService;
+
+    @RequestMapping("/add")
+    public Result add(@RequestBody CheckItem checkItem){
+        try {
+            checkItemService.add(checkItem);
+        }catch (Exception e){
+            return new Result(false, MessageConstant.ADD_CHECKITEM_FAIL);
+        }
+
+        return  new Result(true,MessageConstant.ADD_CHECKITEM_SUCCESS);
+    }
+}
---
+++ health_parent/health_interface/src/main/java/com/cpucode/service/CheckItemService.java
@@ -0,0 +1,16 @@
+package com.cpucode.service;
+
+import com.cpucode.pojo.CheckItem;
+
+/**
+ * 检查项服务接口
+ *
+ * @author : cpucode
+ * @date : 2021/3/29
+ * @time : 13:00
+ * @github : https://github.com/CPU-Code
+ * @csdn : https://blog.csdn.net/qq_44226094
+ */
+public interface CheckItemService {
+    public void add(CheckItem checkItem);
+}
